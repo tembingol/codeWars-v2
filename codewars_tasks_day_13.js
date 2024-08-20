d10a006579596750971c13d128d980ffcfaa09a2
tembingol <tembingol@gmail.com>
2024-08-20 15:45:02 +0500

    add: added 1 solved tasks
    Signed-off-by: tembingol <tembingol@gmail.com>

--- codewars_tasks_day_13.js
+++ codewars_tasks_day_13.js
@@ -141,3 +141,22 @@ function smash(words) {
   return words.join(" ");
 }
 //task 9 ---
+
+//task 10 +++
+/*
+8 kyu
+What's the real floor?
+*/
+function getRealFloor(n) {
+  if (n == 0) return 0;
+
+  if (n > 0 && n < 13) {
+    return n - 1;
+  }
+
+  if (n > 13) {
+    return n - 2;
+  }
+  return n;
+}
+//task 10 ---
