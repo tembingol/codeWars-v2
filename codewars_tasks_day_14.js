4ba7160e491a742bfdcc6cd293b1e7f4fe3d5090
tembingol <tembingol@gmail.com>
2024-08-21 22:27:02 +0500

    add: added 1 solved tasks
    Signed-off-by: tembingol <tembingol@gmail.com>

---
+++ codewars_tasks_day_14.js
@@ -0,0 +1,17 @@
+//task 1 +++
+/*
+6 kyu
+Bouncing Balls
+*/
+function bouncingBall(h, bounce, window) {
+  if (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) return -1;
+
+  let result = 0;
+  // your code here
+  while (h > window) {
+    result += 2;
+    h = h * bounce;
+  }
+  return result - 1;
+}
+//task 1 ---
